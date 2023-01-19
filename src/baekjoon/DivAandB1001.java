1583a143f35e03104c92267e77feaef2ad9948f6
sungbaekheo <sungbaek.heo@gmail.com>
2023-01-19 13:10:32 +0900

    baekjoon problem solving start

---
+++ src/baekjoon/DivAandB1001.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class DivAandB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println((double) a/b);
+    }
+}
---
+++ src/baekjoon/StringAddString10926.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class StringAddString10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.next();
+        System.out.println(id+"??!");
+    }
+}
