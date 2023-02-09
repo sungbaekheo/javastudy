283fd5b7988d24a5220f0ebb8129fac2f9349118
sungbaekheo <sungbaek.heo@gmail.com>
2023-02-09 17:07:21 +0900

    230209 17:07

--- src/baekjoon/silver/NumberChaining2635.java
+++ src/baekjoon/silver/NumberChaining2635.java
@@ -1,6 +1,5 @@
 package baekjoon.silver;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NumberChaining2635 {
@@ -8,13 +7,29 @@ public class NumberChaining2635 {
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
-        
-        int count = 0;
+        int maxCount = 0;
+        String answer = "";
         for(int i=num; i>num/2; i--){
-            if(num>=0){
+            int initialNum = num;
+            int numberChain = i;
+            int count = 0;
+            String numList = "";
+            while(initialNum >= 0){
+                numList += initialNum+" ";
+                int temp = initialNum-numberChain;
+                initialNum = numberChain;
+                numberChain = temp;
                 count++;
-                System.out.print(num+" ");
+            }
+            if(count>maxCount){
+                maxCount = count;
+                answer = numList;
+            }
+            if(count<maxCount){
+                break;
             }
         }
+        System.out.println(maxCount);
+        System.out.println(answer);
     }
 }
