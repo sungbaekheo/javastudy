af24491f3b9c79215c11973f3e73bf4549210903
Sungbaek Heo <heosb123@hanmail.net>
2023-03-03 00:34:52 +0900

    230303 00:34

---
+++ src/baekjoon/silver/Ant10158.java
@@ -0,0 +1,28 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Ant10158 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
+
+        while(t>0){
+            
+        }
+    }
+}
--- src/baekjoon/silver/KoreanMelonFarm2477.java
+++ src/baekjoon/silver/KoreanMelonFarm2477.java
@@ -12,8 +12,8 @@ public class KoreanMelonFarm2477 {
     int w = 0;
     int subArea = 0;
     int[] lens = new int[6];
-    int[] counter = new int[5];
-    int idx = 0;
+    int[] order = new int[6];
+
     for(int p=0; p<6; p++){
       int d = sc.nextInt();
       int len = sc.nextInt();
@@ -22,13 +22,23 @@ public class KoreanMelonFarm2477 {
       } else{
         h = Math.max(h, len);
       }
-      counter[d]++;
+      order[p] = d;
       lens[p] = len;
-      if(counter[d] == 2){
-        idx = p;
-      }
     }
-    subArea = lens[idx-2]*lens[idx-1];
+    if(order[0] == order[2] && order[1] == order[3]){
+      subArea = lens[2]*lens[1];
+    } else if(order[1] == order[3] && order[2] == order[4]){
+      subArea = lens[3]*lens[2];
+    } else if(order[2] == order[4] && order[3] == order[5]){
+      subArea = lens[4]*lens[3];
+    } else if(order[3] == order[5] && order[4] == order[0]){
+      subArea = lens[5]*lens[4];
+    } else if(order[4] == order[0] && order[5] == order[1]){
+      subArea = lens[0]*lens[5];
+    } else if(order[5] == order[1] && order[0] == order[2]){
+      subArea = lens[1]*lens[0];
+    }
+
     area = h*w-subArea;
     System.out.println(melons*area);
   }
