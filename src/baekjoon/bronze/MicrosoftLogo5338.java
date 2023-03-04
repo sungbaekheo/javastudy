3e1d6c3fdd828cc06b86bf75ca573acaf3fcc82b
Sungbaek Heo <heosb123@hanmail.net>
2023-03-04 22:50:39 +0900

    230304 22:50

---
+++ src/baekjoon/bronze/MicrosoftLogo5338.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze;
+
+public class MicrosoftLogo5338 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("       _.-;;-._\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-;;-._|\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-''-._|");
+
+        System.out.println(sb.toString());
+    }
+}
---
+++ src/baekjoon/silver/Sequence2491.java
@@ -0,0 +1,5 @@
+package baekjoon.silver;
+
+public class Sequence2491 {
+    
+}
