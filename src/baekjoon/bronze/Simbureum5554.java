c9f874012814249e85fe0504c84e7d6fe84e1b2f
Sungbaek Heo <heosb123@hanmail.net>
2023-03-07 23:04:45 +0900

    230307 23:04

---
+++ src/baekjoon/bronze/Simbureum5554.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Simbureum5554 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for(int i=0; i<4; i++){
+            sum+=sc.nextInt();
+        }
+        System.out.println(sum/60);
+        System.out.println(sum%60);
+    }
+}
