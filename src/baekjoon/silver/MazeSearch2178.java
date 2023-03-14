de7d73ab67fb6c23b7edf96b35c702ba41e0c277
Sungbaek Heo <sungbaek.heo@gmail.com>
2023-03-14 23:54:05 +0900

    230314 23:54

--- src/baekjoon/silver/MazeSearch2178.java
+++ src/baekjoon/silver/MazeSearch2178.java
@@ -7,12 +7,12 @@ import java.util.Scanner;
 // Compiler version JDK 11.0.2
 
 public class MazeSearch2178 {
-    static int[][] maze;
-    static boolean[][] visited;
-    static int row;
-    static int col;
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static int[][] maze; // 미로~
+    static boolean[][] visited; // 방문처리용 불리언 배열
+    static int row; // 행
+    static int col; // 열
+    static int[] dr = {-1, 1, 0, 0}; // 델타배열
+    static int[] dc = {0, 0, -1, 1}; // 상하좌우
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         row = sc.nextInt();
@@ -23,38 +23,38 @@ public class MazeSearch2178 {
         for(int r=0; r<row; r++){
             char[] rowArray = sc.next().toCharArray();
             for(int c=0; c<col; c++){
-                maze[r][c] = rowArray[c] - '0';
+                maze[r][c] = rowArray[c] - '0'; // 붙어서 입력받으므로 각 인덱스의 캐릭터에서 0을 빼주면 int값
             }
         }
         
-        BFS(0, 0);
+        BFS(0, 0); // 너비우선탐색 고고
         
         System.out.println(maze[row-1][col-1]);
     }
 
     static void BFS(int r, int c){
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(r, c));
-        visited[r][c] = true;
-        while(!q.isEmpty()){
-            Point point = q.poll();
+        Queue<Point> q = new LinkedList<>(); // BFS를 위한 큐
+        q.add(new Point(r, c)); // 첫 시작점 넣고
+        visited[r][c] = true; // 방문 처리
+        while(!q.isEmpty()){ // 큐가 비어있지 않으면
+            Point point = q.poll(); // 큐 맨 앞에 있는 좌표 뽑기
             for(int d=0; d<4; d++){
-                int nr = point.r + dr[d];
+                int nr = point.r + dr[d]; // 상하좌우 탐색하기
                 int nc = point.c + dc[d];
 
-                if(nr<0 || nr>=row || nc<0 || nc>=col){
+                if(nr<0 || nr>=row || nc<0 || nc>=col){ // indexoutofbounds 가 뜨는 좌표는 넘기기
                     continue;
                 }
-                if(visited[nr][nc] == true || maze[nr][nc] == 0){
+                if(visited[nr][nc] == true || maze[nr][nc] == 0){ // 이미 방문했거나 0인 곳은 패스
                     continue;
                 }
-                q.add(new Point(nr, nc));
-                maze[nr][nc]+=maze[point.r][point.c];
-                visited[nr][nc] = true;
+                q.add(new Point(nr, nc)); // 방문하지 않았고 1이라면 큐에 넣기
+                maze[nr][nc]+=maze[point.r][point.c]; // 이전 장소까지의 거리를 다음장소에 더하기(총 거리가 누적됨)
+                visited[nr][nc] = true; // 방문처리
             }
         }
     }
-    static class Point{
+    static class Point{ // 좌표 담을 객체
         int r;
         int c;
         public Point(int r, int c){
---
+++ src/baekjoon/silver/Virus2606.java
@@ -0,0 +1,48 @@
+package baekjoon.silver;
+
+import java.util.Scanner;
+
+ // Compiler version JDK 11.0.2
+
+public class Virus2606{
+    static int[][] graph;
+    static boolean[] visited;
+    static int infected = 0;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    
+        int coms = sc.nextInt();
+        int edge = sc.nextInt();
+    
+        graph = new int[coms+1][coms+1];
+        visited = new boolean[coms+1];
+    
+        for(int i=0; i<edge; i++){
+        int f = sc.nextInt();
+        int r = sc.nextInt();
+            
+        graph[f][r] = graph[r][f] = 1;
+        }
+        
+        DFS(1);
+        
+        System.out.println(infected);
+    }
+    
+    static void DFS(int host){
+        if(visited[host]){
+            return;
+        }
+        
+        visited[host] = true;
+        if(host != 1){
+            infected++;
+        }
+        
+        for(int i=0; i<graph[host].length; i++){
+            if(graph[host][i] == 1){
+                DFS(i);
+            }
+        }
+    }
+}
