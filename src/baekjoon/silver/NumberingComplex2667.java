26e8af56b1e2fc954370126080a714fddd856d6c
Sungbaek Heo <sungbaek.heo@gmail.com>
2023-03-17 01:20:20 +0900

    230317 01:20

--- src/baekjoon/silver/NumberingComplex2667.java
+++ src/baekjoon/silver/NumberingComplex2667.java
@@ -9,14 +9,13 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
-<<<<<<< HEAD
 public class NumberingComplex2667 {
     static int size;
-    static int[][] village;
-    static boolean[][] visited;
-    static int[] dr = {-1, 1, 0, 0};
+    static int[][] village; // 마을 정보 배열
+    static boolean[][] visited; // 방문처리 배열
+    static int[] dr = {-1, 1, 0, 0}; // 상하좌우 델타배열
     static int[] dc = {0, 0, -1, 1};
-    static List<Integer> numList = new ArrayList<>();
+    static List<Integer> numList = new ArrayList<>(); // 단지 수가 얼마나 나오든 유연하게 담을 리스트
     public static void main(String args[]) throws IOException { 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
@@ -31,16 +30,16 @@ public class NumberingComplex2667 {
             }
         }
     
-        int nComplex = 0;
+        int nComplex = 0; // 단지 수
         for(int i=0; i<size; i++){
             for(int j=0; j<size; j++){
                 if(village[i][j] == 1 && !visited[i][j]){
                     BFS(i, j);
-                    nComplex++;
+                    nComplex++; // BFS가 한 번 실행될 때마다 단지 수 증가 처리
                 }
             }
         }
-        Collections.sort(numList);
+        Collections.sort(numList); // 오름차순 정렬
         System.out.println(nComplex);
         for(int i=0; i<nComplex; i++){
             System.out.println(numList.get(i));
@@ -49,94 +48,27 @@ public class NumberingComplex2667 {
     
     static void BFS(int r, int c){
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {r, c});
-        visited[r][c] = true;
+        q.add(new int[] {r, c}); // 시작점 큐에 입력
+        visited[r][c] = true; // 방문처리
         
-        int nHome = 0;
+        int nHome = 0; // 단지에 있는 집의 수
         while(!q.isEmpty()){
-            nHome++;
+            nHome++; 
             int[] point = q.poll();
-            for(int d=0; d<4; d++){
+            for(int d=0; d<4; d++){ // 사방탐색
                 int nr = point[0] + dr[d];
                 int nc = point[1] + dc[d];
                 
-                if(nr<0 || nr>=size || nc<0 || nc>=size){
+                if(nr<0 || nr>=size || nc<0 || nc>=size){ // 배열 범위 이탈하면 리턴
                     continue;
                 }
                 
-                if(village[nr][nc] == 1 && !visited[nr][nc]){
-                    q.add(new int[] {nr, nc});
-                    visited[nr][nc] = true;
+                if(village[nr][nc] == 1 && !visited[nr][nc]){ // 집이 있고 방문한 적이 없다면
+                    q.add(new int[] {nr, nc}); // 큐에 좌표 넣기
+                    visited[nr][nc] = true; // 해당 좌표 방문처리
                 }
             }
         }
-        numList.add(nHome);
+        numList.add(nHome); // 단지 별 집의 개수 리스트에 추가
     }
 }
-=======
- // Compiler version JDK 11.0.2
-
-public class NumberingComplex2667{
-  static int size;
-  static int[][] village;
-  static boolean[][] visited;
-  static int[] dr = {-1, 1, 0, 0};
-  static int[] dc = {0, 0, -1, 1};
-  static List<Integer> numList = new ArrayList<>();
-   public static void main(String args[]) throws IOException { 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    size = Integer.parseInt(br.readLine());
-    village = new int[size][size];
-    visited = new boolean[size][size];
-    
-    for(int i=0; i<size; i++){
-      char[] row = br.readLine().toCharArray();
-      for(int j=0; j<size; j++){
-        village[i][j] = row[j] - '0';
-      }
-    }
-    
-    int nComplex = 0;
-    for(int i=0; i<size; i++){
-      for(int j=0; j<size; j++){
-        if(village[i][j] == 1 && !visited[i][j]){
-          BFS(i, j);
-          nComplex++;
-        }
-      }
-    }
-    Collections.sort(numList);
-    System.out.println(nComplex);
-    for(int i=0; i<nComplex; i++){
-      System.out.println(numList.get(i));
-    }
-   }
-   
-   static void BFS(int r, int c){
-     Queue<int[]> q = new LinkedList<>();
-     q.add(new int[] {r, c});
-     visited[r][c] = true;
-     
-     int nHome = 0;
-     while(!q.isEmpty()){
-       nHome++;
-       int[] point = q.poll();
-       for(int d=0; d<4; d++){
-         int nr = point[0] + dr[d];
-         int nc = point[1] + dc[d];
-         
-         if(nr<0 || nr>=size || nc<0 || nc>=size){
-           continue;
-         }
-         
-         if(village[nr][nc] == 1 && !visited[nr][nc]){
-           q.add(new int[] {nr, nc});
-           visited[nr][nc] = true;
-         }
-       }
-     }
-     numList.add(nHome);
-   }
- }
->>>>>>> 8a127ed72b88a365f69f97743adb73f61aad38e0
--- src/baekjoon/silver/StartLink5014.java
+++ src/baekjoon/silver/StartLink5014.java
@@ -4,48 +4,42 @@ import java.util.Scanner;
 import java.util.Queue;
 import java.util.LinkedList;
 
-public class StartLink5014 {
-  static int total;
-  static int[] floors;
-   public static void main(String[] args){
-     Scanner sc = new Scanner(System.in);
-     
-     total = sc.nextInt();
-     int start = sc.nextInt();
-     int end = sc.nextInt();
-     int up = sc.nextInt();
-     int down = sc.nextInt();
-     
-     floors = new int[total+1];
-     
-     if((Math.abs(start-end)%Math.abs(up-down) !=0)
-     || (start>end && down == 0)
-     || (start<end && up == 0)){
-      System.out.println("use the stairs");
-     } 
-
-     BFS(start, end, up, down); 
-     System.out.println(floors[end]);
-   }
-   
-   static void BFS(int s, int e, int u, int d){
-     Queue<Integer> q = new LinkedList<>();
-     q.add(s);
-     int count = 0;
-     while(!q.isEmpty()){
-       int curr = q.poll();
-       if(floors[curr] == 0){
-         floors[curr] = count;
-       }
-       if(curr == e){
-         return;
-       }
-       if(e > curr && curr-d>=1){
-         q.add(curr+u);
-       } else if(e < curr && curr+u<=total) {
-         q.add(curr-d);
-       }
-       count++;
-     }
-   }
- }
+public class StartLink5014{
+  static int total; // 층
+  static int[] floors; // 층별 도착하기까지 최소 횟수
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    
+    total = sc.nextInt();
+    int start = sc.nextInt();
+    int end = sc.nextInt();
+    int up = sc.nextInt();
+    int down = sc.nextInt();
+    
+    floors = new int[total+1];
+    
+    BFS(start, end, up, down);   
+  }   
+  
+  static void BFS(int s, int e, int u, int d){
+    Queue<Integer> q = new LinkedList<>();
+    q.add(s); // 시작 층 큐에 넣고
+    floors[s] = 1; // 방문처리 겸 횟수 입력
+    while(!q.isEmpty()){
+      int curr = q.poll();
+      if(curr == e){ // 현재 층이 목표 층이면
+        System.out.println(floors[curr]-1); // 저장된 회수에서 1을 빼고 출력
+        return;
+      }
+      if(curr+u<=total && floors[curr+u] == 0){ // 더 이상 위로 갈 수 없을 때가 아니거나 방문한 적이 없으면
+        q.add(curr+u); // 위로 간 경우의 층 큐에 넣기
+        floors[curr+u] = floors[curr]+1; // 방문 처리 겸 횟수 입력
+      }
+      if(curr-d>0 && floors[curr-d] == 0) { // 더 이상 아래로 내려갈 수 없을 때가 아니거나 방문하지 않았다면
+        q.add(curr-d); // 아래로 간 경우의 층 큐에 넣기
+        floors[curr-d] = floors[curr]+1; // 방문 처리 겸 횟수 입력
+      }
+    }
+    System.out.println("use the stairs"); // 큐가 다 빌 때까지 목표층에 도달하지 못하면 출력
+  }
+}
