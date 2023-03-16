60634fa122ab15757d761e2960b486560c9f1d28
Sungbaek Heo <sungbaek.heo@gmail.com>
2023-03-16 09:01:11 +0900

    230316 09:00

---
+++ src/baekjoon/silver/NumberingComplex2667.java
@@ -0,0 +1,76 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+ // Compiler version JDK 11.0.2
+
+public class NumberingComplex2667{
+  static int size;
+  static int[][] village;
+  static boolean[][] visited;
+  static int[] dr = {-1, 1, 0, 0};
+  static int[] dc = {0, 0, -1, 1};
+  static List<Integer> numList = new ArrayList<>();
+   public static void main(String args[]) throws IOException { 
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    size = Integer.parseInt(br.readLine());
+    village = new int[size][size];
+    visited = new boolean[size][size];
+    
+    for(int i=0; i<size; i++){
+      char[] row = br.readLine().toCharArray();
+      for(int j=0; j<size; j++){
+        village[i][j] = row[j] - '0';
+      }
+    }
+    
+    int nComplex = 0;
+    for(int i=0; i<size; i++){
+      for(int j=0; j<size; j++){
+        if(village[i][j] == 1 && !visited[i][j]){
+          BFS(i, j);
+          nComplex++;
+        }
+      }
+    }
+    Collections.sort(numList);
+    System.out.println(nComplex);
+    for(int i=0; i<nComplex; i++){
+      System.out.println(numList.get(i));
+    }
+   }
+   
+   static void BFS(int r, int c){
+     Queue<int[]> q = new LinkedList<>();
+     q.add(new int[] {r, c});
+     visited[r][c] = true;
+     
+     int nHome = 0;
+     while(!q.isEmpty()){
+       nHome++;
+       int[] point = q.poll();
+       for(int d=0; d<4; d++){
+         int nr = point[0] + dr[d];
+         int nc = point[1] + dc[d];
+         
+         if(nr<0 || nr>=size || nc<0 || nc>=size){
+           continue;
+         }
+         
+         if(village[nr][nc] == 1 && !visited[nr][nc]){
+           q.add(new int[] {nr, nc});
+           visited[nr][nc] = true;
+         }
+       }
+     }
+     numList.add(nHome);
+   }
+ }
---
+++ src/baekjoon/silver/StartLink5014.java
@@ -0,0 +1,49 @@
+package baekjoon.silver;
+
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class StartLink5014 {
+  static int total;
+  static int[] floors;
+   public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+     
+     total = sc.nextInt();
+     int start = sc.nextInt();
+     int end = sc.nextInt();
+     int up = sc.nextInt();
+     int down = sc.nextInt();
+     
+     floors = new int[total+1];
+     
+     if((Math.abs(start-end)%Math.abs(up-down) !=0)
+     || (start>end && down == 0)
+     || (start<end && up == 0)){
+      System.out.println("use the stairs");
+     } 
+
+     BFS(start, end, up, down); 
+   }
+   
+   static void BFS(int s, int e, int u, int d){
+     Queue<Integer> q = new LinkedList<>();
+     q.add(s);
+     int count = 0;
+     while(!q.isEmpty()){
+       int curr = q.poll();
+       if(floors[curr] == 0){
+         floors[curr] = count;
+       }
+       if(curr == e){
+         return;
+       }
+       if(e > curr || curr-d<1){
+         q.add(curr+u);
+       } else if(e < curr || curr+u>total) {
+         q.add(curr-d);
+       }
+     }
+   }
+ }
