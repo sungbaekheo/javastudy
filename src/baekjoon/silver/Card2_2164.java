32430b1033b7729b0541221808b9497996445566
Sungbaek Heo <sungbaek.heo@gmail.com>
2023-03-20 17:57:58 +0900

    230320 17:56

--- src/baekjoon/silver/Card2_2164.java
+++ src/baekjoon/silver/Card2_2164.java
@@ -1,7 +1,23 @@
 package baekjoon.silver;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Card2_2164 {
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
+    Queue<Integer> q = new LinkedList<>();
+
+    int n = sc.nextInt();
+    for(int i=1; i<=n; i++){
+      q.add(i);
+    }
+
+    while(!(q.size() == 1)){
+      q.poll();
+      q.add(q.poll());
+    }
+    System.out.println(q.poll());
   }
 }
