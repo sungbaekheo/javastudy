5b8e965f4fd42d6bad8de5e8f63ebb83fee7b57d
sungbaekheo <sungbaek.heo@gmail.com>
2023-04-15 01:03:19 +0900

    230415 01:03

--- src/baekjoon/gold/GoingDown2096.java
+++ src/baekjoon/gold/GoingDown2096.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 내려가기 {
+public class GoingDown2096 {
     public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
