d2409dbcc61706e6a06294783d68b3e063085567
sungbaekheo <sungbaek.heo@gmail.com>
2023-04-25 14:21:24 +0900

    230425 14:21

--- src/baekjoon/gold/SetMirrors2151.java
+++ src/baekjoon/gold/SetMirrors2151.java
@@ -1,16 +1,13 @@
 package baekjoon.gold;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class SetMirrors2151 {
     static int size;
     static char[][] home;
     static boolean[][][] visited;
-    static Position[] doors = new Position[2];
-    static int nMirror;
-    static int minNum;
+    
     static int[] dr = {-1, 0, 1, 0}; // 상우하좌
     static int[] dc = {0, 1, 0, -1};
 
@@ -24,49 +21,33 @@ public class SetMirrors2151 {
             home[r] = sc.next().toCharArray();
         }
 
-        nMirror = 0;
-        int doorCnt = 0;
         for(int r=0; r<size; r++){
             for(int c=0; c<size; c++){
                 if(home[r][c] == '#'){
-                    int dir = 0;
-                    if(r == 0){
-                        dir = 2;
-                    } else if(r == size-1){
-                        dir = 0;
-                    } else if(c == 0){
-                        dir = 1;
-                    } else if(c == size-1){
-                        dir = 3;
-                    }
-                    doors[doorCnt++] = new Position(r, c, 0, dir);
-                } else if(home[r][c] == '!'){
-                    nMirror++;
+                    BFS(new Position(r, c, 0, 0));
+                    return;
                 }
             }
         }
-        visited = new boolean[size][size][nMirror+1];
-
-        BFS(doors[0]);
-
-        System.out.println(minNum);
     }
 
     public static void BFS(Position start){
-        Queue<Position> q = new LinkedList<>();
-        q.offer(start);
-        visited[start.r][start.c][start.n] = true;
+        PriorityQueue<Position> q = new PriorityQueue<>();
+        visited = new boolean[size][size][4];
+        int r = start.r;
+        int c = start.c;
+        for(int i=0; i<4; i++){
+            q.offer(new Position(r, c, 0, i));
+            visited[r][c][i] = true;
+        }
 
-        minNum = Integer.MAX_VALUE;
         while(!q.isEmpty()){
             Position curr = q.poll();
-            
-            if(curr.n>=minNum){
-                continue;
-            }
+            visited[curr.r][curr.c][curr.d] = true;
 
-            if(curr.r == doors[1].r && curr.c == doors[1].c){
-                minNum = Math.min(minNum, curr.n);
+            if(!(curr.r == start.r && curr.c == start.c) && home[curr.r][curr.c] == '#'){
+                System.out.println(curr.n);
+                return;
             }
 
             int nr = curr.r + dr[curr.d];
@@ -76,27 +57,19 @@ public class SetMirrors2151 {
                 continue;
             }
 
-            if(home[nr][nc] == '*' || visited[nr][nc][curr.n]){
+            if(home[nr][nc] == '*' || visited[nr][nc][curr.d]){
                 continue;
             }
 
-            if(home[nr][nc] == '!' && curr.n<nMirror){
-                visited[nr][nc][curr.n+1] = true;
-                visited[nr][nc][curr.n] = true;
+            q.offer(new Position(nr, nc, curr.n, curr.d));
+            if(home[nr][nc] == '!'){
                 q.offer(new Position(nr, nc, curr.n+1, (curr.d+4-1)%4));
                 q.offer(new Position(nr, nc, curr.n+1, (curr.d+1)%4));
-                q.offer(new Position(nr, nc, curr.n, curr.d));
-            } else if(home[nr][nc] == '.'){
-                visited[nr][nc][curr.n] = true;
-                q.offer(new Position(nr, nc, curr.n, curr.d));
-            } else{
-                visited[nr][nc][curr.n] = true;
-                q.offer(new Position(nr, nc, curr.n, curr.d));
             }
         }
     }
 
-    static class Position {
+    static class Position implements Comparable<Position>{
         int r;
         int c;
         int n;
@@ -107,6 +80,10 @@ public class SetMirrors2151 {
             this.c = c;
             this.n = n;
             this.d = d;
+        }
+
+        public int compareTo(Position o) {
+            return this.n - o.n;
         }
     }
 }
